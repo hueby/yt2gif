1314d7b33ea25d96f9146836724c8721e27bde0d
hueby <dennis.huebner89gmail.com>
2016-10-31 18:39:32 +0100

    Impletemented single- and multiplechoice logic for simulating  selecting answers

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express = require('express'), app = express(),  router = require('./router')
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
-
 router(app);
 
 var port = process.env.PORT || 8080;
@@ -13,3 +12,24 @@ var port = process.env.PORT || 8080;
 app.listen(port);
 module.exports = app;
 console.log('Listening on localhost:' + port);
+
+/*var nock = require('nock');
+/var youtube = nock('http://www.youtube.com').filteringPath(function() {
+  return '/';
+})
+.get('/')
+.reply(200, { status: 'nock works' });
+//var youtube = nock('http://localhost:8080').get('/getInfo/?').reply(200, { status: 'nock works'});
+console.log(youtube);*/
+
+/**
+ * nock("http://www.google.com")
+    .filteringPath(function(path){
+        return '/';
+    })
+    .get("/")
+    .reply(200, "this should work?");
+
+ * 
+ */
+
--- controllers/_videoController.js
+++ controllers/_videoController.js
@@ -1,13 +1,21 @@
-youtubedl = require('youtube-dl');
+var youtubedl = require('youtube-dl'), nock = require('nock');
 
-exports.getInfo = function(req, res, next) {
+var youtube = nock('http://www.youtube.com').filteringPath(function() {
+  return '/';
+})
+.get('/')
+.reply(200, { status: 'nock works' });
+
+exports.getInfo = function(req, res) {
   var url = 'https://www.youtube.com/watch?v=' + req.params.url;
   youtubedl.getInfo(url, [],  function(err, info) {
+    console.log(err);
+    console.log('INFOOO ' + info.status);
     if (err) {
       res.json({
         status: 404,
-        title: "Not found",
-        duration: "Invalid URL"
+        title: 'Not found',
+        duration: 'Invalid URL'
       });
     } else {
       res.status(200).json({
@@ -21,12 +29,10 @@ exports.getInfo = function(req, res, next) {
 
 // check if seconds are well formatted
 function getDuration(duration) {
-  exp = duration.split(':');
+  var exp = duration.split(':');
   return (exp[1].length === 1 ? exp[0] + ':0' + exp[1] : duration);
 }
 
-exports.getVideo = function(req, res, next) {
+exports.getVideo = function(req, res) {
   res.status(404);
 };
-
-
